4b0447cb9dd83326e3e36d9ee6309fba02e76910
Nicholas Carrara <ncarrara.physics@gmail.com>
2021-10-28 11:21:57 -0700

    stuff

---
+++ NeutronExtractor_module.cc
@@ -0,0 +1,64 @@
+/**
+ * @file    NeutronExtractor_module.cc
+ * @brief   A module for extracting truth/reco information about G4 particle trajectories
+ *          and conducting some standard analysis tasks. 
+ *          Generated at Mon Oct 11 11:21:12 2021 using cetskelgen
+ * @ingroup NeutronExtractor
+ * @author  Nicholas Carrara (nmcarrara@ucdavis.edu),
+ *          Jingbo Wang
+ *          Junying Haung
+ *          Yashwanth Bezawada
+ *          Walker Johnson
+**/
+
+// art framework includes
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Principal/SubRun.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "canvas/Utilities/InputTag.h"
+#include "canvas/Utilities/Exception.h"
+#include "canvas/Persistency/Common/FindManyP.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+
+// special utility includes
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Table.h"
+#include "art_root_io/TFileService.h"
+
+
+// LArSoft includes
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/GeometryCore.h"
+#include "larcoreobj/SimpleTypesAndConstants/geo_types.h"
+#include "lardata/DetectorInfoServices/DetectorClocksService.h"
+#include "lardataobj/Simulation/SimEnergyDeposit.h"
+#include "lardataobj/RecoBase/PFParticle.h"
+#include "lardataobj/RecoBase/Cluster.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Track.h"
+#include "lardataobj/RecoBase/SpacePoint.h"
+#include "lardataobj/RecoBase/Slice.h"
+#include "lardataobj/Simulation/SimChannel.h"
+#include "larsim/Simulation/LArG4Parameters.h"
+#include "nusimdata/SimulationBase/MCParticle.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
+
+// necessary ROOT libraries
+#include <TTree.h>
+#include <TH1.h>
+#include "TH1F.h"
+#include "TGeoMaterial.h"
+#include "TGeoElement.h"
+
+// C includes
+#include <cmath>
+
+
+
+DEFINE_ART_MODULE(lar::NeutronExtractor)
