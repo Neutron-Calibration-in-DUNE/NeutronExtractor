624db26a25d9c0e9f989485a5e7069e7d8ef3c5a
Nicholas Carrara <ncarrara.physics@gmail.com>
2021-11-04 13:00:23 -0700

    some changes

--- MCNeutron.cxx
+++ MCNeutron.cxx
@@ -19,6 +19,27 @@ namespace neutron
         fNeutronTrackId.emplace_back(particle.TrackId());
         fNeutronParentId.emplace_back(particle.Mother());
         fNeutronNumberOfTrajectoryPoints.emplace_back(particle.NumberTrajectoryPoints());
+        std::vector<Double_t> T; std::vector<Double_t> X; std::vector<Double_t> Y; std::vector<Double_t> Z;
+        std::vector<Double_t> E; std::vector<Double_t> Px; std::vector<Double_t> Py; std::vector<Double_t> Pz;
+        for (Int_t i = 0; i < particle.NumberTrajectoryPoints(); i++)
+        {
+            T.emplace_back(particle.T(i));
+            X.emplace_back(particle.Vx(i));
+            Y.emplace_back(particle.Vy(i));
+            Z.emplace_back(particle.Vz(i));
+            E.emplace_back(particle.E(i));
+            Px.emplace_back(particle.Px(i));
+            Py.emplace_back(particle.Py(i));
+            Pz.emplace_back(particle.Pz(i));
+        }
+        fNeutronT.emplace_back(T);
+        fNeutronX.emplace_back(X);
+        fNeutronY.emplace_back(Y);
+        fNeutronZ.emplace_back(Z);
+        fNeutronE.emplace_back(E);
+        fNeutronPx.emplace_back(Px);
+        fNeutronPy.emplace_back(Py);
+        fNeutronPz.emplace_back(Pz);
     }
 
     void MCNeutron::FillTTree()
@@ -44,12 +65,29 @@ namespace neutron
         fMCNeutronTree->Branch("track_id", &mc_neutron.track_id);
         fMCNeutronTree->Branch("parent_id", &mc_neutron.parent_id);
         fMCNeutronTree->Branch("number_of_trajectory_points", &mc_neutron.number_of_trajectory_points);
+        fMCNeutronTree->Branch("t", &mc_neutron.t);
+        fMCNeutronTree->Branch("x", &mc_neutron.x);
+        fMCNeutronTree->Branch("y", &mc_neutron.y);
+        fMCNeutronTree->Branch("z", &mc_neutron.z);
+        fMCNeutronTree->Branch("E", &mc_neutron.E);
+        fMCNeutronTree->Branch("px", &mc_neutron.px);
+        fMCNeutronTree->Branch("py", &mc_neutron.py);
+        fMCNeutronTree->Branch("pz", &mc_neutron.pz);
         // iterate over all neutrons
         for (size_t i = 0; i < fNumberOfNeutrons; i++)
         {
             mc_neutron.event_id = fEventId[i];
             mc_neutron.track_id = fNeutronTrackId[i];
+            mc_neutron.parent_id = fNeutronParentId[i];
             mc_neutron.number_of_trajectory_points = fNeutronNumberOfTrajectoryPoints[i];
+            mc_neutron.t = fNeutronT[i];
+            mc_neutron.x = fNeutronX[i];
+            mc_neutron.y = fNeutronY[i];
+            mc_neutron.z = fNeutronZ[i];
+            mc_neutron.E = fNeutronE[i];
+            mc_neutron.px = fNeutronPx[i];
+            mc_neutron.py = fNeutronPy[i];
+            mc_neutron.pz = fNeutronPz[i];
             fMCNeutronTree->Fill();
         }
     }
