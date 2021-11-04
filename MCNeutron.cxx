7f37991a2d1f918c13019467f0f9d4434c4225c2
Nicholas Carrara <ncarrara.physics@gmail.com>
2021-11-04 13:26:10 -0700

    some changes

--- MCNeutron.cxx
+++ MCNeutron.cxx
@@ -21,7 +21,7 @@ namespace neutron
         fNeutronStatusCode.emplace_back(particle.StatusCode());
         fNeutronNumberOfDaughters.emplace_back(particle.NumberDaughters());
         std::vector<Int_t> daughters = {};
-        for (size_t i = 0; i < particle.NumberDaughters(); i++)
+        for (Int_t i = 0; i < particle.NumberDaughters(); i++)
         {
             daughters.emplace_back(particle.Daughter(i));
         }
