564635273aa45e0069243a2a55c1172afbdac001
Nick Carrara <ncarrara.physics@gmail.com>
2022-02-09 11:47:34 -0800

    some changes

--- Voxelizer.cxx
+++ Voxelizer.cxx
@@ -89,7 +89,7 @@ namespace neutron
             neutron_x, neutron_y, neutron_z, neutron_edep_ids, 0);
         Voxels muonVoxels = generateVoxels(event, voxelSize,
             muon_x, muon_y, muon_z, muon_edep_ids, 1);
-        std::cout << "HERE" << std::endl;
+        
         
         neutronVoxels.values = neutron_edep_energy;
         muonVoxels.values = muon_edep_energy;
@@ -97,6 +97,7 @@ namespace neutron
         // consolidate edep energy values
         neutronVoxels.consolidate(discretizeFeatures);
         muonVoxels.consolidate(discretizeFeatures);
+        std::cout << "HERE" << std::endl;
         
         for (size_t i = 0; i < neutronVoxels.x_id.size(); i++)
         {
