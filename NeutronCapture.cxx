94ba85ff7407b64d8533fa8fde73229530eee0c0
Nicholas Carrara <ncarrara.physics@gmail.com>
2022-05-25 07:51:23 -0700

    some changes

--- NeutronCapture.cxx
+++ NeutronCapture.cxx
@@ -49,10 +49,8 @@ namespace neutron
     )
     {
         NeutronCaptureReco NeutronCaptureReco;
-        std::cout << "HERE" << std::endl;
         if (mcParticles.isValid() and mcChannels.isValid() and recoSpacePoints.isValid())
         {
-            std::cout << "HERE AGAIN" << std::endl;
             /**
              * We first iterate through all particles and create a map of 
              * parent-daughter pairs for track ids.  This way we can search
